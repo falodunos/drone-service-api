810c15ff29b46dcc93e52363a947bad2c902c28e
Falodun O. Solomon <falodunosolomon@gmail.com>
2021-12-30 18:58:16 +0100

    ... initial commit of project with little changes ...

---
+++ src/main/java/com/drone/manager/DroneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.drone.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneServiceApplication.class, args);
+	}
+
+}
